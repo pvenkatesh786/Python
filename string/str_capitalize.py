58aed5daa20147df7780a0c48a9647774a4760f3
root <padalavenkatesh786@gmail.com>
2016-06-14 05:09:00 +0000

    String

---
+++ string/str_capitalize.py
@@ -0,0 +1,6 @@
+#!/bin/python
+## Sytnax :--> str.capitalize(), this will print the first letter of first word in capital letter in the line, rest of the words remain same
+
+str = "hi this is venky"
+print "str.capitalize() :",str.capitalize()
+
